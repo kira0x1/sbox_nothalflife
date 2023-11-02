261c8708a27c43092c44f67629f33338565bb9da
Kira <kira.eva@protonmail.com>
2023-11-02 20:55:02 +0000

    Added ammo

---
+++ code/ui/gunhud/GunHud.cs
@@ -0,0 +1,25 @@
+﻿using Sandbox;
+using Sandbox.UI;
+using Sandbox.UI.Construct;
+
+namespace MyGame
+{
+	partial class GunHud : Panel
+	{
+		public int CurAmmo;
+		public int MagazineSize;
+		public int HeldAmmoLeft;
+
+
+		public override void Tick()
+		{
+			if ( !Game.IsClient ) return;
+			Pawn player = (Pawn)Game.LocalPawn;
+			Weapon activeWeapon = player.ActiveWeapon;
+			CurAmmo = activeWeapon.CurrentAmmo;
+			MagazineSize = activeWeapon.MagazineSize;
+			HeldAmmoLeft = activeWeapon.HeldAmmoLeft;
+			StateHasChanged();
+		}
+	}
+}
---
+++ code/weapon/Smg.cs
@@ -0,0 +1,39 @@
+﻿using Sandbox;
+
+namespace MyGame
+{
+	public partial class Smg : Weapon
+	{
+		public override string ModelPath => "weapons/rust_smg/rust_smg.vmdl";
+		public override string ViewModelPath => "weapons/rust_smg/v_rust_smg.vmdl";
+
+		public override float PrimaryRate => 15.0f;
+
+		public override int MagazineSize => 40;
+
+		public override int MaxHeldAmmo => 260;
+
+		[ClientRpc]
+		protected virtual void ShootEffects()
+		{
+			Game.AssertClient();
+
+			Particles.Create( "particles/pistol_muzzleflash.vpcf", EffectEntity, "muzzle" );
+
+			Pawn.SetAnimParameter( "b_attack", true );
+			ViewModelEntity?.SetAnimParameter( "fire", true );
+		}
+
+		public override void PrimaryAttack()
+		{
+			ShootEffects();
+			Pawn.PlaySound( "rust_smg.shoot" );
+			ShootBullet( 0.2f, 170, 45, 1.5f );
+		}
+
+		protected override void Animate()
+		{
+			Pawn.SetAnimParameter( "holdtype", (int)CitizenAnimationHelper.HoldTypes.Rifle );
+		}
+	}
+}
